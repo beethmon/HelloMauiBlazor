aad1fd2c561af1d3bafb6a0e7ba3836838e06488
皓文 罗 <beethmon@gmail.com>
2024-10-15 19:02:53 +0800

    添加项目文件。

---
+++ HelloMauiBlazor/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloMauiBlazor
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
